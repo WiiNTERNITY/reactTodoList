3ad3461513410838a8b3ced84debfbafc7f8b638
wiinternity <hammani_mounir@outlook.fr>
2023-01-24 16:30:40 +0100

    to do liste

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import TaskList from './components/TaskList.js';
+import './App.css';
+
+const TASKS = [
+  {
+    id: 1,
+    title: 'Mow the lawn',
+    isComplete: false,
+  },
+  {
+    id: 2,
+    title: 'Cook Pasta',
+    isComplete: true,
+  },
+];
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Ada&apos;s Task List</h1>
+      </header>
+      <main>
+        <div>{<TaskList tasks={TASKS} />}</div>
+      </main>
+    </div>
+  );
+};
+
+export default App;
